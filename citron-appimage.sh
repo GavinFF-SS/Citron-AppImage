0a9b5d5803569615afa2b7e33213f97f5da63cca
GavinFF-SS <gavin.a.cowden1@gmail.com>
2025-03-23 00:04:25 -0400

    steam deck
    fr

--- citron-appimage.sh
+++ citron-appimage.sh
@@ -9,19 +9,8 @@ REPO="https://git.citron-emu.org/Citron/Citron.git"
 LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
 URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
 
-if [ "$ARCH" = 'x86_64' ]; then
-	if [ "$1" = 'v3' ]; then
-		echo "Making x86-64-v3 optimized build of citron"
-		ARCH="${ARCH}_v3"
-		ARCH_FLAGS="-march=x86-64-v3 -O3"
-	else
-		echo "Making x86-64 generic build of citron"
-		ARCH_FLAGS="-march=x86-64 -mtune=generic -O3"
-	fi
-else
-    echo "Making znver2 (Steam Deck) build of citron"
-	ARCH_FLAGS="-march=znver2 -mtune=znver2"
-fi
+echo "Making znver2 (Steam Deck) build of citron"
+ARCH_FLAGS="-march=znver2 -mtune=znver2"
 
 UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
 
@@ -49,35 +38,6 @@ fi
 	#Replaces 'boost::asio::io_service' with 'boost::asio::io_context' for compatibility with Boost.ASIO versions 1.74.0 and later
 	find src -type f -name '*.cpp' -exec sed -i 's/boost::asio::io_service/boost::asio::io_context/g' {} \;
 
-if [ "$ARCH" = 'x86_64' ]; then
-    mkdir build
-	cd build
-	cmake .. -GNinja \
-		-DCITRON_USE_BUNDLED_VCPKG=OFF \
-		-DCITRON_USE_BUNDLED_QT=OFF \
-		-DUSE_SYSTEM_QT=ON \
-		-DCITRON_USE_BUNDLED_FFMPEG=OFF \
-		-DCITRON_USE_BUNDLED_SDL2=ON \
-		-DCITRON_USE_EXTERNAL_SDL2=OFF \
-		-DCITRON_TESTS=OFF \
-		-DCITRON_CHECK_SUBMODULES=OFF \
-		-DCITRON_USE_LLVM_DEMANGLE=OFF \
-		-DCITRON_ENABLE_LTO=ON \
-		-DCITRON_USE_QT_MULTIMEDIA=ON \
-		-DCITRON_USE_QT_WEB_ENGINE=OFF \
-		-DENABLE_QT_TRANSLATION=ON \
-		-DUSE_DISCORD_PRESENCE=OFF \
-		-DBUNDLE_SPEEX=ON \
-		-DCITRON_USE_FASTER_LD=OFF \
-		-DCMAKE_INSTALL_PREFIX=/usr \
-		-DCMAKE_CXX_FLAGS="$ARCH_FLAGS -Wno-error" \
-		-DCMAKE_C_FLAGS="$ARCH_FLAGS" \
-		-DCMAKE_SYSTEM_PROCESSOR="$(uname -m)" \
-		-DCMAKE_BUILD_TYPE=Release
-	ninja
-	sudo ninja install
-	echo "$VERSION" >~/version
-else
     mkdir build && cd build
     cmake .. -GNinja \
         -DCITRON_ENABLE_LTO=ON \
@@ -94,7 +54,6 @@ else
     ninja
     sudo ninja install
     echo "$VERSION" >~/version
-fi
 )
 rm -rf ./citron
 VERSION="$(cat ~/version)"
